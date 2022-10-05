4d7f6239aaf01e776f798e5aa9f61ffd64e3de6e
Ryuke <sahalrajiv-extc@atharvacoe.ac.in>
2022-10-05 20:29:01 +0530

    Added tailwind files

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+        hind: ["Hind", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
